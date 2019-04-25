5bad014ebbf3dbbbab7a7a835f36df1b5e13de7a
SAM <sam@SAMs-MacBook-Pro.local>
2019-04-25 20:29:48 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+
+
+
+using namespace std;
+
+int main()
+{
+int ix,jx;
+
+    for (int i =1 ; i<=5 ; i++)
+            for (int j=1 ;j<=5 ; j++){
+            int t;
+            cin>>t;
+            if(t){cout <<abs(i-3)+abs(j-3);
+            break;
+            }
+            break;}
+
+               return 0;
+}
+
+
+
+            /*
+            if(t){
+                ix=i ,  jx=j;}  }
+
+ ix= 3-ix;
+ jx=3-jx;
+
+ if(ix<0)
+ ix=ix * -1;
+
+ if (jx<0)
+ jx=jx * -1;
+ cout<<jx+ix;
+    return 0;
+}
+*/
